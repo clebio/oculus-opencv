8a7b7c0b4780d0582fd4d4476c9058594ef45eee
Caleb Hyde <chyde@alightanalytics.com>
2014-12-25 09:40:38 -0600

    Refactor key-metric code; add crops

--- oculus-stream.py
+++ oculus-stream.py
@@ -1,27 +1,26 @@
 """
-Oculus Video: streams two camera feeds into the Oculus Rift after having distorted
-    them to account for pincushion effect
+Oculus Video: streams two camera feeds into the Oculus Rift after
+  having distorted them to account for pincushion effect
 
 Will display on whichever monitor is current when run.
 
 Requires OpenCV 2.4.9, numpy 1.8.1 and ovrsdk
 
-Based directly off of http://www.argondesign.com/news/2014/aug/26/augmented-reality-oculus-rift/
+Based directly off of: http://www.argondesign.com/news/2014/aug/26/augmented-reality-oculus-rift/
 """
 
-
+import sys
 import cv2
 import numpy as np
-import time
-from ovrsdk import *
+import ovrsdk as ovr
 
-def crop(image, xL, xR, yL, yR, width, height):
-    return image[xL:width-xR, yL:height-yR]
+def crop(image, _xl, _xr, _yl, _yr, width, height):
+    return image[_xl:width-_xr, _yl:height-_yr]
 
-def createDistortionMatrix(fx, cx, fy, cy):
+def create_distortion_matrix(_fx, _cx, _fy, _cy):
     matrix = np.array([
-        [fx, 0, cx],
-        [0, fy, cy],
+        [_fx, 0, _cx],
+        [0, _fy, _cy],
         [0, 0, 1]
     ])
     return matrix
@@ -33,11 +32,15 @@ def transform(image, matrix):
             [0.0, 200, 95],
             [0.0, 0.0, 1]
         ])
-    imageDis = cv2.undistort(image, matrix, np.array([0.22, 0.24, 0, 0, 0]))
-    return imageDis
+    image_distortion = cv2.undistort(
+        image,
+        matrix,
+        np.array([0.22, 0.24, 0, 0, 0])
+    )
+    return image_distortion
 
-def joinImages(imageL, imageR):
-    return np.append(imageL, imageR, axis=1)
+def join_images(image_left, image_right):
+    return np.append(image_left, image_right, axis=1)
 
 def translate(image, x, y):
     """Oculus DK2 is two images together equal to 2364 x 1461
@@ -47,15 +50,10 @@ def translate(image, x, y):
     """
     rows, cols = 576, 720 #288, 384
     matrix = np.float32([[1, 0, x], [0, 1, y]])
-    imageT = cv2.warpAffine(image, matrix, (cols, rows))
-    return imageT
+    image_translate = cv2.warpAffine(image, matrix, (cols, rows))
+    return image_translate
 
 if __name__ == '__main__':
-    t1 = -1
-    t2 = -1
-    startTime = time.clock()
-    lastTime = time.clock()
-
     #Matrix coefficients for left eye barrel effect
     fxL = 300
     fyL = 200
@@ -75,40 +73,74 @@ if __name__ == '__main__':
     yR = -yL
 
     #offsets to translate image before distortion
-    xO = 0
-    yO = 0
+    xo = 0
+    yo = 0
 
     #offsets to translate image after distortion
-    xO2 = -70
-    yO2 = 40
+    xo2 = -70
+    yo2 = 40
 
+    cropXL = 0
+    cropXR = 200
+    cropYL = 0
+    cropYR = 0
 
-    ESCAPE = ''
-    window = 0
+    key_mappings = dict(
+        fxL=('f', 's'),
+        fxR=('f', 's'),
+        fyL=('e', 'd'),
+        fyR=('e', 'd'),
+        cxL=('l', 'j'),
+        cxR=('l', 'j'),
+        cyL=('k', 'i'),
+        cyR=('k', 'i'),
+        yo2=('o', 'u'),
+        xo2=('m', 'n'),
+        xo=('.', ','),
+        yo=('h', ';'),
+        cropXL=('z', 'x'),
+        cropYL=('w', 'r'),
+        cropXR=('c', 'v'),
+        cropYR=('a', 'g'),
+    )
 
     cR = cv2.VideoCapture(0)
     cL = cv2.VideoCapture(1)
 
+    if not (cR.isOpened() and cL.isOpened()):
+        print('Failed to find two cameras. Are they connected?')
+        sys.exit()
+
     """initializes ovrsdk and starts tracking oculus"""
-    ovr_Initialize()
-    hmd = ovrHmd_Create(0)
+    ovr.ovr_Initialize()
+    hmd = ovr.ovrHmd_Create(0)
     try:
         hmd.contents
     except ValueError as _ex:
         print('Failed to initialize Oculus, is it connected?')
         sys.exit()
 
-    hmdDesc = ovrHmdDesc()
-    ovrHmd_GetDesc(hmd, byref(hmdDesc))
-    ovrHmd_StartSensor(
+    hmdDesc = ovr.ovrHmdDesc()
+    ovr.ovrHmd_GetDesc(hmd, ovr.byref(hmdDesc))
+    ovr.ovrHmd_StartSensor(
         hmd,
-        ovrSensorCap_Orientation | ovrSensorCap_YawCorrection,
+        ovr.ovrSensorCap_Orientation | ovr.ovrSensorCap_YawCorrection,
         0
     )
 
     # The device dimensions; should we use them for width/height?
-    size0 = ovrHmd_GetFovTextureSize(hmd, ovrEye_Left, hmdDesc.MaxEyeFov[0], 1.0)
-    size1 = ovrHmd_GetFovTextureSize(hmd, ovrEye_Left, hmdDesc.MaxEyeFov[1], 1.0)
+    size0 = ovr.ovrHmd_GetFovTextureSize(
+        hmd,
+        ovr.ovrEye_Left,
+        hmdDesc.MaxEyeFov[0],
+        1.0
+    )
+    size1 = ovr.ovrHmd_GetFovTextureSize(
+        hmd,
+        ovr.ovrEye_Left,
+        hmdDesc.MaxEyeFov[1],
+        1.0
+    )
 
     cv2.namedWindow('vid', 16 | cv2.WND_PROP_FULLSCREEN)
     cv2.setWindowProperty(
@@ -117,93 +149,64 @@ if __name__ == '__main__':
         cv2.cv.CV_WINDOW_FULLSCREEN
     )
 
-    cropXL = 0
-    cropXR = 200
-    cropYL = 0
-    cropYR = 0
-
     while True:
-        x,fraL = cL.read()
-        x,fraR = cR.read()
+        x, fraL = cL.read()
+        x, fraR = cR.read()
         width, height = 720, 480 #, t = fraL.shape
-        matrixL = createDistortionMatrix(fxL, cxL, fyL, cyL)
-        matrixR = createDistortionMatrix(fxR, cxR, fyR, cyR)
+        matrixL = create_distortion_matrix(fxL, cxL, fyL, cyL)
+        matrixR = create_distortion_matrix(fxR, cxR, fyR, cyR)
         #translates, crops and distorts image
-        fraLT = translate(fraL, xL+xO, yL+yO)
-        fraRT = translate(fraR, xR+xO, yR+yO)
+        fraLT = translate(fraL, xL+xo, yL+yo)
+        fraRT = translate(fraR, xR+xo, yR+yo)
         fraLd = transform(fraLT, matrixL)
         fraRd = transform(fraRT, matrixR)
-        fraLT2 = translate(fraLd, xO2, yO2)
-        fraRT2 = translate(fraRd, xO2, yO2)
-        fraLs = crop(fraLT2, cropXL, cropXR, cropYL, cropYR, width, height)
-        fraRs = crop(fraRT2, cropXL, cropXR, cropYL, cropYR, width, height)
-        fraCom = joinImages(fraLs, fraRs)
-        cv2.imshow('vid',fraCom)
+        fraLT2 = translate(fraLd, xo2, yo2)
+        fraRT2 = translate(fraRd, xo2, yo2)
+        fraLs = crop(
+            fraLT2, cropXL, cropXR, cropYL, cropYR, width, height
+        )
+        fraRs = crop(
+            fraRT2, cropXL, cropXR, cropYL, cropYR, width, height
+        )
 
-        key =  cv2.waitKey(1) & 255
+        fraCom = join_images(fraLs, fraRs)
+        cv2.imshow('vid', fraCom)
 
+        key = cv2.waitKey(1) & 255
         if key == ord('q'):
             cv2.destroyAllWindows()
             cR.release()
             cL.release()
+            print(("fxL={fxl}, fxR={fxr}, cxL={cxl}, cxR={cxr}, "
+                   "xo={xo}, yo={yo}, xo2={xo2}, yo2={yo2}").format(
+                       fxl=fxL,
+                       fxr=fxR,
+                       cxl=cxL,
+                       cxr=cxR,
+                       xo=xo,
+                       yo=yo,
+                       xo2=xo2,
+                       yo2=yo2,
+                   ))
             break
 
-        elif key == ord('e'):
-            fyL += 10
-            fyR += 10
-        elif key == ord('d'):
-            fyL += -10
-            fyR += -10
-        elif key == ord('s'):
-            fxL += -10
-            fxR += -10
-        elif key == ord('f'):
-            fxL += 10
-            fxR += 10
-        elif key == ord('i'):
-            cyL += -10
-            cyR += -10
-        elif key == ord('k'):
-            cyL += 10
-            cyR += 10
-        elif key == ord('j'):
-            cxL += -10
-            cxR += -10
-        elif key == ord('l'):
-            cxL += 10
-            cxR += 10
+        for metric, tup in key_mappings.iteritems():
+            _add = tup[0]
+            _sub = tup[1]
+            if key == ord(_add):
+                locals()[metric] += 10
+            if key == ord(_sub):
+                locals()[metric] -= 10
 
-        elif key == ord('o'):
-            yO2 += 10
-        elif key == ord('u'):
-            yO2 += -10
+        # Don't let these go negative
+        for metric in ['cropXL', 'cropYL', 'cropXR', 'cropYR']:
+            if locals()[metric] < 0:
+                print("Attempting to set {} below zero".format(
+                    metric
+                ))
+                locals()[metric] = 0
 
-        elif key == ord('m'):
-            xO2 += 10
-        elif key == ord('n'):
-            xO2 += -10
 
-        elif key == ord(','):
-            xO += -10
-        elif key == ord('.'):
-            xO += 10
-        elif key == ord('h'):
-            yO += 10
-        elif key == ord(';'):
-            yO += -10
-
-        elif key == ord('p'):
-            print("fxL={fxl}, fxR={fxr}, cxL={cxl}, cxR={cxr}, xO={xo}, yO={yo}, xO2={xo2}, yO2={yo2}".format(
-                fxl=fxL,
-                fxr=fxR,
-                cxl=cxL,
-                cxr=cxR,
-                xo=xO,
-                yo=yO,
-                xo2=xO2,
-                yo2=yO2,
-                )
-            )
-        # fxL=330, fxR=330, cxL=320, cxR=320, xO=0, yO=10, xO2=-80, yO2=60
-        # fxL=350, fxR=350, cxL=210, cxR=210, xO=-20, yO=40, xO2=-50, yO2=-10
-        # fxL=330, fxR=330, cxL=320, cxR=320, xO=0, yO=10, xO2=-80, yO2=60
+# fxL=330, fxR=330, cxL=320, cxR=320, xo=0, yo=10, xo2=-80, yo2=60
+# fxL=350, fxR=350, cxL=210, cxR=210, xo=-20, yo=40, xo2=-50, yo2=-10
+# fxL=330, fxR=330, cxL=320, cxR=320, xo=0, yo=10, xo2=-80, yo2=60
